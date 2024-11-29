218dd718f12b38c8f478a45cce1be2098ae73828
Karim Abdelkareem <132012097+Karim0690@users.noreply.github.com>
2024-11-30 01:09:32 +0200

    some setups

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,90 +1,144 @@
-import React from 'react'
+import { ChevronDown, Menu } from "lucide-react";
 export default function Header() {
   return (
-    
-    
-
-<nav class="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
-  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
-        <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
-        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-    </a>
-    <button data-collapse-toggle="navbar-dropdown" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-dropdown" aria-expanded="false">
-        <span class="sr-only">Open main menu</span>
-        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-        </svg>
-    </button>
-    <div class="hidden w-full md:block md:w-auto" id="navbar-dropdown">
-      <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-        <li>
-          <a href="#" class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent" aria-current="page">Home</a>
-        </li>
-        <li>
-            <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" class="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">Dropdown <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
-  </svg></button>
-            {/* <!-- Dropdown menu --> */}
-            <div id="dropdownNavbar" class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
-                <ul class="py-2 text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdownLargeButton">
+    <nav className=" border-gray-200 bg-black  dark:border-gray-700">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <span className="self-center text-3xl whitespace-nowrap text-white font-bold">
+          Limtless Logistics
+        </span>
+        <button
+          data-collapse-toggle="navbar-dropdown"
+          type="button"
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          aria-controls="navbar-dropdown"
+          aria-expanded="false"
+        >
+          <span className="sr-only">Open main menu</span>
+          <Menu />
+        </button>
+        <div className="hidden w-full md:block md:w-auto" id="navbar-dropdown">
+          <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg  rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 ">
+            <li>
+              <a
+                href="#"
+                className="block py-2 px-3 text-white"
+                aria-current="page"
+              >
+                تتبع شحنتك
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block py-2 px-3 text-white"
+                aria-current="page"
+              >
+                تعرف علي اقرب فرع
+              </a>
+            </li>
+            <li>
+              <button
+                id="dropdownNavbarLink"
+                data-dropdown-toggle="dropdownNavbar"
+                className="flex items-center justify-between w-full py-2 px-3 text-white"
+              >
+                الخدمات <ChevronDown />
+              </button>
+              {/* <!-- Dropdown menu --> */}
+              <div
+                id="dropdownNavbar"
+                className="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600"
+              >
+                <ul
+                  className="py-2 text-white"
+                  aria-labelledby="dropdownLargeButton"
+                >
                   <li>
-                    <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
+                    <a href="#" className="block px-4 py-2 text-sm text-white">
+                      التجاره الالكترونيه
+                    </a>
                   </li>
                   <li>
-                    <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
+                    <a href="#" className="block px-4 py-2 text-sm text-white">
+                      شحن البضائع
+                    </a>
                   </li>
                   <li>
-                    <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
+                    <a href="#" className="block px-4 py-2 text-sm text-white">
+                      الشحن والتوزيع
+                    </a>
                   </li>
                 </ul>
-                <div class="py-1">
-                  <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
+                <div className="py-1">
+                  <a href="#" className="block px-4 py-2 text-sm text-white">
+                    التوزيع الداخلي
+                  </a>
                 </div>
-            </div>
-        </li>
-        <li>
-          <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
-        </li>
-        <li>
-          <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
-        </li>
-        <li>
-          <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+              </div>
+            </li>
 
-  )
+            <li>
+              <button
+                id="dropdownNavbarLink"
+                data-dropdown-toggle="dropdownNavbar"
+                className="flex items-center justify-between w-full py-2 px-3 text-white"
+              >
+                الدعم و المساعده <ChevronDown />
+              </button>
+              {/* <!-- Dropdown menu --> */}
+              <div
+                id="dropdownNavbar"
+                className="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600"
+              >
+                <ul
+                  className="py-2 text-white"
+                  aria-labelledby="dropdownLargeButton"
+                >
+                  <li>
+                    <a href="#" className="block px-4 py-2 text-sm text-white">
+                      الدعم و المساعده
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="block px-4 py-2 text-sm text-white">
+                      الاسئله الشاثعه
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="block px-4 py-2 text-sm text-white">
+                      اتصل بنا
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-
-
-
-
 // <div className="container">
-       
-    //     <nav>
-    //         <ul className='no-underline flex'>
-    //             <li><a className="text-3xl" href="#">Limitless logestics</a></li>
-    //             <li><a href="#">تتبع شحنتك</a></li>
-    //             <li><a href="#">تعرف علي اقرب فرع</a></li>
-    //             <select name="الخدمات" id="serves">
-    //               <option value="#">الخدمات</option>
-    //               <option value="1">التجاره الالكترونيه</option>
-    //               <option value="2">شحن البضائع</option>
-    //               <option value="3">الشحن والتوزيع</option>
-    //               <option value="4">التوزيع الداخلي</option>
-    //             </select>
-    //             <select name="الدعم و المساعده" id="">
-    //               <option value="1">الدعم و المساعده</option>
-    //               <option value="2">الاسئله الشاثعه</option>
-    //               <option value="3">اتصل بنا</option>
-    //             </select>
-    //         </ul>
-    //     </nav>
 
-      
-    // </div>
+//     <nav>
+//         <ul className='no-underline flex'>
+//             <li><a className="text-3xl" href="#">Limitless logestics</a></li>
+//             <li><a href="#">تتبع شحنتك</a></li>
+//             <li><a href="#">تعرف علي اقرب فرع</a></li>
+//             <select name="الخدمات" id="serves">
+//               <option value="#">الخدمات</option>
+//               <option value="1">التجاره الالكترونيه</option>
+//               <option value="2">شحن البضائع</option>
+//               <option value="3">الشحن والتوزيع</option>
+//               <option value="4">التوزيع الداخلي</option>
+//             </select>
+//             <select name="الدعم و المساعده" id="">
+//               <option value="1">الدعم و المساعده</option>
+//               <option value="2">الاسئله الشاثعه</option>
+//               <option value="3">اتصل بنا</option>
+//             </select>
+//         </ul>
+//     </nav>
+
+// </div>
